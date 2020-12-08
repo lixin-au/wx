76091e50bc6381bacb0c580ddbb86b08e485baa2
Lixin Yang <lsrob@hotmail.com>
2020-12-08 19:08:03 +1100

    Created project.

---
+++ WX.Api/Controllers/MainController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace WX.Api.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        private readonly ILogger<MainController> _logger;
+
+        public MainController(ILogger<MainController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("time")]
+        public IActionResult GetTime()
+        {
+            return Ok(DateTimeOffset.UtcNow);
+        }
+    }
+}
